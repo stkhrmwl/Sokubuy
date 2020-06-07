59244ab1bbbc43343edeb6ec5ce9719954f68c13
TAKAHARA <>
2020-06-07 22:09:51 +0900

    signin to firebase with twitterauth

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, SafeAreaView, TouchableOpacity, Text } from 'react-native';
+import { useTwitter } from 'react-native-simple-twitter';
+import firebase from 'firebase';
+
+// authkeys
+import ENV from '../../env.json';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,16 +15,40 @@ const styles = StyleSheet.create({
 });
 
 export default HomeScreen = (props) => {
-  const handleSubmit = () => {
+  const { navigation } = props;
+
+  const { twitter, TWModal } = useTwitter({
+    onSuccess: (user, accessToken) => {
+      console.log(user);
+      console.log(accessToken);
+      navigation.navigate('Home');
+    },
+  });
+
+  const handleSubmit = async () => {
     console.log('signin');
+    try {
+      await twitter.login();
+      const credential = firebase.auth.TwitterAuthProvider.credential(
+        ENV.TWITTER_ACCESS_TOKEN,
+        ENV.TWITTER_ACCESS_TOKEN_SECRET
+      );
+      return firebase.auth().signInWithCredential(credential);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  const { navigation } = props;
+  useEffect(() => {
+    twitter.setConsumerKey(ENV.TWITTER_API_KEY, ENV.TWITTER_API_SECRET_KEY);
+  });
+
   return (
     <SafeAreaView style={styles.container}>
       <TouchableOpacity onPress={handleSubmit}>
         <Text>button</Text>
       </TouchableOpacity>
+      <TWModal />
     </SafeAreaView>
   );
 };
